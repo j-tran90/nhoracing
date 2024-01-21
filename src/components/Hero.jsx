42ebc1a430fd2c06e6164f5e2fd91570226ab1b1
j-tran90 <johnt90@live.com>
2024-01-20 19:56:02 -0500

    fixed hero font

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,10 @@ export default function Hero() {
   return (
     <>
       <div className="hero-image">
-        <div className="container">
-          <div className="hero-section">R32 GODZILLA</div>
-          <div className="container-fluid gradient-up"></div>
+        <div className="container-fluid hero-section text-center">
+          R32 GODZILLA
         </div>
+        <div className="container-fluid gradient-up"></div>
       </div>
     </>
   );
