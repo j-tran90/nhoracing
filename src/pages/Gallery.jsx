9db239c1ed6f16d0a2336cc4d374ec9c75b7f73f
j-tran90 <johnt90@live.com>
2024-01-22 18:13:21 -0500

    testing with reduce gallery images

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -3,119 +3,115 @@ import "../stylesheets/Gallery.css";
 export default function Gallery() {
   return (
     <>
-      <div className="">
-        <div className="gallery-image">
-          <div className="container-fluid hero-section text-center">
-            GALLERY
+      <div className="gallery-image">
+        <div className="container-fluid hero-section text-center">GALLERY</div>
+        <div className="container-fluid gradient-up"></div>
+      </div>
+      <div className="container" id="gallery">
+        <div className="row row-cols-md-2 row-cols-lg-4">
+          <div className="col4 brighten">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/IMG_0797.JPG?alt=media&token=7c06f9d6-ee13-4cc6-93b6-a30d7859083f"
+              className="crop responsive"
+            />
           </div>
-          <div className="container-fluid gradient-up"></div>
-        </div>
-        <div className="container" id="gallery">
-          <div className="row row-cols-md-2 row-cols-lg-4">
-            <div className="col4 brighten">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/IMG_0797.JPG?alt=media&token=7c06f9d6-ee13-4cc6-93b6-a30d7859083f"
-                className="crop responsive"
-              />
-            </div>
-            <div className="col4 brighten">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/r32_stock.jpeg?alt=media&token=00394648-dcfd-4e5c-a04b-6aafa11459ae"
-                className="crop responsive"
-              />
-            </div>
-            <div className="col4 brighten">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/11090968_973323522726819_1988416038673955572_o.jpg?alt=media&token=5a01963d-aa4b-47cb-9934-04c6843976f6"
-                className="crop responsive"
-              />
-            </div>
-            <div className="col4 brighten">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/R32%20GTR%20at%20Sandpoint.jpg?alt=media&token=32d744c6-c549-4379-8e76-e2b4e3bc21fc"
-                className="crop responsive"
-              />
-            </div>
+          <div className="col4 brighten">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/r32_stock.jpeg?alt=media&token=00394648-dcfd-4e5c-a04b-6aafa11459ae"
+              className="crop responsive"
+            />
           </div>
-          <div className="row row-cols-md-2 row-cols-lg-4">
-            <div className="col4 brighten">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/IMG_0586.jpg?alt=media&token=49c92ee9-b1b8-4b30-b0bd-5440546608bb"
-                className="crop responsive"
-              />
-            </div>
-            <div className="col4 brighten">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/FB_IMG_1585501177433.jpg?alt=media&token=b77f8ea4-aa7b-4ee9-991a-1f618fda1c1f"
-                className="crop responsive"
-              />
-            </div>
-            <div className="col4 brighten">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/FB_IMG_1588651985053.jpg?alt=media&token=f543aa91-fe6a-40d4-af0c-e43cd213a21a"
-                className="crop responsive"
-              />
-            </div>
-            <div className="col4 brighten">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/AZN%20Renegade-02538.jpg?alt=media&token=a9eb000e-174a-46f8-ae08-d4ecd3e8e8ea"
-                className="crop responsive"
-              />
-            </div>
+          <div className="col4 brighten">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/11090968_973323522726819_1988416038673955572_o.jpg?alt=media&token=5a01963d-aa4b-47cb-9934-04c6843976f6"
+              className="crop responsive"
+            />
           </div>
-          <div className="row row-cols-md-2 row-cols-lg-4">
-            <div className="col4 brighten">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/brian%20r32.jpg?alt=media&token=44134527-99f6-4543-8ce7-0f55d7e5f7ff"
-                className="crop responsive"
-              />
-            </div>
-            <div className="col4 brighten">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/FB_IMG_1570744612431.jpg?alt=media&token=660da233-c539-4026-9c25-ca2d432cc7b5"
-                className="crop responsive"
-              />
-            </div>
-            <div className="col4 brighten">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/Brian_s_R32-5.jpg?alt=media&token=1207d7ae-93d3-465d-85c8-47c258cb5113"
-                className="crop responsive"
-              />
-            </div>
-            <div className="col4 brighten">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/79BE8109-579A-4E90-86AB-0FED2BE07BFE.JPEG?alt=media&token=4c9161d0-174f-4a0e-a75e-2ede0c2cfd57"
-                className="crop responsive crop-position"
-              />
-            </div>
-          </div>
-          <div className="row row-cols-md-2 row-cols-lg-4">
-            <div className="col4 brighten">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/Brian's%20R32%20GTR-2.jpg?alt=media&token=269fc918-192e-41cd-91c4-2cd8c6b2e41d"
-                className="crop responsive crop-position"
-              />
-            </div>
-            <div className="col4 brighten">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/Brian_Nho's_R32_Downtown_RiversideFlag-16.jpg?alt=media&token=d5dc1096-2ab9-49a3-873c-7e94a8f28906"
-                className="crop responsive"
-              />
-            </div>
-            <div className="col4 brighten">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/IMG_9171.jpg?alt=media&token=4e01719b-f905-41e3-81a7-2ea458cd1e4c"
-                className="crop responsive"
-              />
-            </div>
-            <div className="col4 brighten">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/FB_IMG_1562028389024.jpg?alt=media&token=ff296eeb-7723-4dd7-a1c1-eaf9738bc68f"
-                className="crop responsive"
-              />
-            </div>
+          <div className="col4 brighten">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/R32%20GTR%20at%20Sandpoint.jpg?alt=media&token=32d744c6-c549-4379-8e76-e2b4e3bc21fc"
+              className="crop responsive"
+            />
           </div>
         </div>
+        {/* <div className="row row-cols-md-2 row-cols-lg-4">
+          <div className="col4 brighten">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/IMG_0586.jpg?alt=media&token=49c92ee9-b1b8-4b30-b0bd-5440546608bb"
+              className="crop responsive"
+            />
+          </div>
+          <div className="col4 brighten">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/FB_IMG_1585501177433.jpg?alt=media&token=b77f8ea4-aa7b-4ee9-991a-1f618fda1c1f"
+              className="crop responsive"
+            />
+          </div>
+          <div className="col4 brighten">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/FB_IMG_1588651985053.jpg?alt=media&token=f543aa91-fe6a-40d4-af0c-e43cd213a21a"
+              className="crop responsive"
+            />
+          </div>
+          <div className="col4 brighten">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/AZN%20Renegade-02538.jpg?alt=media&token=a9eb000e-174a-46f8-ae08-d4ecd3e8e8ea"
+              className="crop responsive"
+            />
+          </div>
+        </div>
+        <div className="row row-cols-md-2 row-cols-lg-4">
+          <div className="col4 brighten">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/brian%20r32.jpg?alt=media&token=44134527-99f6-4543-8ce7-0f55d7e5f7ff"
+              className="crop responsive"
+            />
+          </div>
+          <div className="col4 brighten">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/FB_IMG_1570744612431.jpg?alt=media&token=660da233-c539-4026-9c25-ca2d432cc7b5"
+              className="crop responsive"
+            />
+          </div>
+          <div className="col4 brighten">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/Brian_s_R32-5.jpg?alt=media&token=1207d7ae-93d3-465d-85c8-47c258cb5113"
+              className="crop responsive"
+            />
+          </div>
+          <div className="col4 brighten">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/79BE8109-579A-4E90-86AB-0FED2BE07BFE.JPEG?alt=media&token=4c9161d0-174f-4a0e-a75e-2ede0c2cfd57"
+              className="crop responsive crop-position"
+            />
+          </div>
+        </div>
+        <div className="row row-cols-md-2 row-cols-lg-4">
+          <div className="col4 brighten">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/Brian's%20R32%20GTR-2.jpg?alt=media&token=269fc918-192e-41cd-91c4-2cd8c6b2e41d"
+              className="crop responsive crop-position"
+            />
+          </div>
+          <div className="col4 brighten">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/Brian_Nho's_R32_Downtown_RiversideFlag-16.jpg?alt=media&token=d5dc1096-2ab9-49a3-873c-7e94a8f28906"
+              className="crop responsive"
+            />
+          </div>
+          <div className="col4 brighten">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/IMG_9171.jpg?alt=media&token=4e01719b-f905-41e3-81a7-2ea458cd1e4c"
+              className="crop responsive"
+            />
+          </div>
+          <div className="col4 brighten">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/FB_IMG_1562028389024.jpg?alt=media&token=ff296eeb-7723-4dd7-a1c1-eaf9738bc68f"
+              className="crop responsive"
+            />
+          </div>
+        </div> */}
       </div>
     </>
   );
